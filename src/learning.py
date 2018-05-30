5137737c0e4a4eef420e29240857b86a5d31db87
lowyiiii <mr.lowbattery@gmail.com>
2018-05-30 18:20:55 -0400

    sends for the controller the distance from the line and the angle between the drone and the line

---
+++ src/learning.py
@@ -0,0 +1,110 @@
+#!/usr/bin/python
+
+'''
+
+__Author_ = Lowyi
+__Email__ = MR.LowBattery@gmail.com
+__Team__  = MRL_UAV
+
+'''
+
+
+import roslib
+
+roslib.load_manifest('bebop_line_follower')
+import sys
+import rospy
+import numpy as np
+import cv2
+import time
+from std_msgs.msg import String, Int32
+from geometry_msgs.msg import Quaternion
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+from math import *
+
+image_exist = 0
+
+
+class image_receiver:
+    ''' Class used in the modes in which camera is required '''
+
+    def __init__(self, camera=1):
+        self.best_x = 0
+        self.camera = camera
+        self.bridge = CvBridge()
+        self.subscribed = 0
+        self.lower = np.array([5, 50, 50], dtype="uint8")
+        self.upper = np.array([15, 255, 255], dtype="uint8")
+        # self.lower = np.array([110, 50, 50], dtype="uint8")
+        # self.upper = np.array([130, 255, 255], dtype="uint8")
+        # self.lower = np.array([10, 70, 50], dtype="uint8")
+        # self.upper = np.array([180, 255, 255], dtype="uint8")
+        self.contours = []
+        self.kernelOpen = np.ones((5, 5))
+        self.kernelClose = np.ones((20, 20))
+        self.image_pos_pub = rospy.Publisher("data", Quaternion, queue_size=10) #change topic nmae
+
+    def follow_line(self, camera_image):
+        # sends for the controller the distance from the line and the angle between the drone and the line
+        global image_exist
+        height, width = camera_image.shape[:2]
+        crop_img = camera_image[0:150, 0:width]
+        mask = cv2.inRange(crop_img, self.lower, self.upper)
+        im2, contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        one_color_image = cv2.bitwise_and(crop_img, crop_img, mask=mask)
+
+        best_x, best_y = width / 2, height + 1
+        worst_x, worst_y = width / 2, -1
+        detecting = 0
+        for i in contours:
+            detecting = 1
+            (x, y), radius = cv2.minEnclosingCircle(i)
+            if y < best_y:
+                best_y = y
+                best_x = x
+            if y > worst_y:
+                worst_y = y
+                worst_x = x
+        if best_y != worst_y:
+            ang = float(-((best_x - worst_x) / (best_y - worst_y)))
+        else:
+            ang = 0
+        self.worst_x = (worst_x - width / 2)
+        cv2.circle(one_color_image, (int(best_x), int(best_y)), 5, (0, 255, 0), -1)
+        cv2.circle(one_color_image, (int(worst_x), int(worst_y)), 5, (0, 255, 0), -1)
+        self.image_pos_pub.publish(self.worst_x, detecting, atan(ang), self.camera)
+        one_color_image = cv2.resize(one_color_image, (0, 0), fx=0.7, fy=0.7)
+        crop_img = cv2.resize(crop_img, (0, 0), fx=0.7, fy=0.7)
+        cv2.imshow("Image window", np.hstack([one_color_image, crop_img]))
+        image_exist = 1
+        cv2.waitKey(3)
+
+    def callback(self, data):
+
+        # selects between modes
+        try:
+            cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+            self.follow_line(cv_image)
+        except CvBridgeError as e:
+            print (e);
+
+    def __del__(self):
+        pass
+ic = None
+ic = image_receiver(1)
+
+def callback(data):
+    ic.callback(data)
+def main(args):
+    rospy.init_node('image_receiver', anonymous=True)
+    rospy.Subscriber("/bebop/image_raw", Image,callback)
+    try:
+        rospy.spin()
+    except KeyboardInterrupt:
+        print("Shutting Down")
+        cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    main(sys.argv)
